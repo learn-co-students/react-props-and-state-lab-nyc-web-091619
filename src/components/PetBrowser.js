9682c586caae58d431a05c30b1ae1412cddb168f
Liam <liam.healy1@marist.edu>
2019-11-20 15:26:21 -0500

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,19 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+  
+  // Should receive a pet prop. Use the attributes in this data to render the pet 
+  // card correctly. It should show the pet's name, type, age and weight.
+  // Based on the pet's gender, the component also needs to contain either a male (♂) or 
+  // female (♀) symbol.
+
+  renderPets = () => {
+    console.log(this.props.pets);
+    return this.props.pets.map(pet => <Pet key={pet.id} pet={pet} onAdoptPet={this.props.onAdoptPet} />)
+  }
+  
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+  return <div className="ui cards">{this.renderPets()}</div>
   }
 }
 
